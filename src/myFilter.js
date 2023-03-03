824049078c11204624fde7c896595272774dbfd7
AlcorBBX <lev.matisov55@gmail.com>
2023-03-03 18:24:57 +0500

    add filter

--- src/myFilter.js
+++ src/myFilter.js
@@ -13,6 +13,11 @@ module.exports = function () {
       }
 
       let result = []
+      for(let i = 0; i < this.length; i++){
+        if(callback(this[i], i, this)) {
+          result.push(this[i]);
+        }
+      }
 
       return result;
     };
--- test/myFilter.test.js
+++ test/myFilter.test.js
@@ -2,8 +2,44 @@ const expect = require("chai").expect;
 
 const pollyfill = require("../src/myFilter");
 
-describe("Testing myFlat pollyfill", () => {
+describe("Testing myFilter pollyfill", () => {
   before(() => {
     pollyfill();
+  });
+
+
+  describe("Testing myFilter", () => {
+    it("Check — wrong type", () => {
+      function badFn() {
+        Array.prototype.myFilter.call(true, (value) => value);
+      }
+      expect(badFn).to.throw(TypeError);
+    });
+
+    it("Check — no callback", () => {
+      function badFn() {
+        [1, 2, 3].myFilter();
+      }
+      expect(badFn).to.throw(TypeError);
+    });
+
+    it("Check — simple loop", () => {
+      expect([1, 2, 3, 4].filter((a, b) => a - b)).to.deep.equal(
+        [1, 2, 3, 4].filter((a, b) => a - b)
+      );
+    });
+
+    it("Check — string", () => {
+      expect(
+        Array.prototype.myFilter.call(["test", "air"], (a, b) => a + b)
+      ).to.deep.equal(Array.prototype.filter.call(["test", "air"], (a,b) => a + b));
+    });
+
+    // it("Check — properties", () => {
+    //   const arr = [1, 2, 3, 4];
+    //   expect(arr.myFilter((item, index, arr) => [item, index, arr])).to.deep.equal(
+    //     arr.filter((item, index, arr) => [item, index, arr])
+    //   );
+    // });
   });
 });
